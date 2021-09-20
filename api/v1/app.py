e2999f10c1e6a7445c4526ea29bf932a1487b4ff
root <root@vagrant.example.com>
2021-09-20 22:46:42 +0000

    started api

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import environ
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -20,11 +20,5 @@ def teardown(exc):
 
 
 if __name__ = "__main__":
-    if api_host is None and api_port is None:
-        app.run(host='0.0.0.0', port='5000', threaded=True)
-    elif api_host is not None and api_port is None:
-        app.run(host=api_host, port='5000', threaded=True)
-    elif api_host is None and api_port is not None:
-        app.run(host='0.0.0.0', port=api_port, threaded=True)
-    else:
-        app.run(host=api_host, port=api_port, threaded=True)
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(getenv('HBNB_API_PORT', '5000')))
